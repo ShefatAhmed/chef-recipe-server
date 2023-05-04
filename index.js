65fe71cc35f3386b5cabfb14b968b40d66fa7479
unknown <shefata69@gmail.com>
2023-05-04 19:06:34 +0600

    ratings

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 const data = require('./data/data.json');
-
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('Chef is running')
 });
 
 app.get('/data', (req, res) => {
     res.send(data)
+});
+
+app.get('/data/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const detailData = data.filter(n => n.id === id);
+    res.send(detailData)
 });
 
 app.listen(port, () => {
